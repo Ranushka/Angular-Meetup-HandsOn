f9ad4a6d4e4bcb20158714e9c4a889198e896129
AngularJsMeetUp <roshan@embla.asia>
2016-04-04 10:38:04 +0530

    Added main code

---
+++ app/speakers/speakers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from 'angular2/core'
+
+
+@Component(
+    {
+        selector: 'speakers',
+        templateUrl: 'app/speakers/speakers.component.html',
+        styleUrls: ['app/speakers/speakers.component.css']
+
+    }
+)
+export class SpeakersComponent{
+    
+}
+
